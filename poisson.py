a71622a6f3082665a9b45e5d4cdf851b627e2a3f
Hirish Chandrasekaran <hirish@ucsb.edu>
2024-11-07 14:12:09 -0600

    Need to test jac_smooth

--- poisson.py
+++ poisson.py
@@ -7,10 +7,22 @@ def get_poisson_fd_3d(n):
     Ax = scipy.sparse.diags(diagonals, offsets, format='csc')
 
     A2D = scipy.sparse.kron(np.eye(n), Ax) + scipy.sparse.kron(Ax, np.eye(n))
-    A3D = scipy.sparse.kron(np.eye(n), A2D)  + scipy.sparse.kron(A2D,np.eye(n))
+    A3D = scipy.sparse.kron(np.eye(n), A2D)  + scipy.sparse.kron(Ax, np.eye(2*n)) 
 
     return A3D
 
+def get_poisson_fd_3d_jacobi(n):
+    diagonals = [np.ones(n-1), 0*np.ones(n), np.ones(n-1)]
+    offsets = np.array([-1, 0, 1])
+    Ax = scipy.sparse.diags(diagonals, offsets, format='csc')
+
+    A2D = scipy.sparse.kron(np.eye(n), Ax) + scipy.sparse.kron(Ax, np.eye(n))
+    A3D = scipy.sparse.kron(np.eye(n), A2D)  + scipy.sparse.kron(Ax, np.eye(2*n)) 
+
+    return A3D
+
+A3D = get_poisson_fd_3d_jacobi(2)
+print(A3D.todense())
 
 
 
