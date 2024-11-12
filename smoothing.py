194e2454dd2537318a540433faf6abc43d6deafe
Hirish Chandrasekaran <hirish@ucsb.edu>
2024-11-12 16:07:27 -0600

    Update smoothing.py

--- smoothing.py
+++ smoothing.py
@@ -1,23 +1,32 @@
 import numpy as np
 from poisson import get_poisson_fd_3d
+import scipy
 
-def jac_smoothing(f, h, nsmooth, A):
+
+def jac_smoothing(f, h, nsmooth, A, n):
     omega = 2/3
-    di = 6/h**2
-    uk = 0
+    di = h**2/6
+    uk = np.zeros(n**3)
     r = f - A @ uk
     for i in range(nsmooth):    
-        uk = uk + omega * 1/di * r
+        uk = uk + omega * di * r
         r = f - A @ uk
     return uk
 
 def test_smoother():
-    n = 4
+    n = 8
     h = 1/(n+1)
     A = get_poisson_fd_3d(n) * 1/h**2
-    #print(A.shape)
-    ue = np.random.rand(n)
+    #k = np.linspace(0,1,n)
+    #V_1D = np.sin((h*np.pi)*np.outer(k,k))
+    #V = np.sqrt(2*h) * np.sqrt(2*h) * np.sqrt(2*h) * scipy.sparse.kron(scipy.sparse.kron(V_1D,V_1D),V_1D)
+    ue = np.random.rand(n**3)
+    #ue = V @ ue
     b = A @ ue
+
+    us = jac_smoothing(b, h, 3, A, n)
+    err = np.linalg.norm(b - A@us)
+    print(err)
 
     
 
