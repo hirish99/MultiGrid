8c832ad3fcfdf42bf7ec1fe9a2441032f0b19163
ryan42210 <ryan_095@foxmail.com>
2024-11-12 16:49:55 -0600

    fix interp_mat

--- jac_smooth.m
+++ jac_smooth.m
@@ -1,13 +1,23 @@
 hdr;
 
-n=200; h=1/(n+1); h2i=1./(h*h);
+n=3; h=1/(n+1); h2i=1./(h*h);
 
 x = h*[1:n]';
-e = ones(n,1);
-A = h2i*spdiags([-e 2*e -e], -1:1, n, n);
+e = ones(n^3,1);
+A = spdiags([-e 2*e -e], -1:1, n, n);
+
+A_2d = kron(A, eye(n)) + kron(eye(n), A);
+A_3d = kron(A, eye(n^2)) + kron(eye(n), A_2d);
+% disp(full(A_3d))
+
+A_3d = h2i * A_3d;
+
+di = 6 / h^2;
 
 k = [1:n]';
 V = sqrt(2*h)*sin((h*pi)*(k*k'));
+V_3d = kron(V, kron(V, V));
+
 Lam = (2*h2i)*(1-cos(h*pi*k));
 lmax = 2;
 lmin = 0.6;
@@ -15,45 +25,43 @@ lmin = 0.6;
 di = h*h/2;
 LS = di*Lam;
 
-ue=0*x;
-ue=rand(n,1);
-ue=V*e;
+
+nc = n/2;
+xc = [1:nc]'/(nc+1);
+J = lin_interp_mat([0; x; 1],[0; xc; 1]);
+J = J(2:end-1,:);
+% Aci = inv(J'*A*J);
+
+J_3d = kron(J, kron(J, J));
+Aci_3d = inv(J_3d'*A_3d*J_3d);
+
+% ue=0*x;
+ue=rand(n^3,1);
+ue=V_3d*e;
 
 
-b = A*ue; u=0*b; nsmooth=4; omega=2/3;
+b = A_3d*ue;
+u=0*b; nsmooth=4; omega=2/3;
 
 r=b;
 for k=1:20
-   e=ue-u; eh=V'*e; uh=V'*u;
-   %plot(LS,0*LS,'k-',LS,eh,'r-',lw,3);
-   %title(['Jac: $k$ = ' num2str([k-1 n]) ', $\omega$ = ' num2str(omega)],intp,ltx,fs,22);
-   %xlabel(['$\lambda(D^{-1}A)$'],intp,ltx,fs,18);
-   %ylabel(['${\hat e_k}/\|{\underline u}\|$'],intp,ltx,fs,18);
-
-   %pause(.05 + .95/k);
-   %plot(LS,0*LS,'k-',LS,eh,'b-',lw,3); hold on;
+   e=ue-u;
 
 %  u=u+cheb_smooth(r,lmax,lmin,nsmooth,A);
    for nu=1:nsmooth;
       u=u+omega*di*r;
-      r=b-A*u;
+      r=b-A_3d*u;
    end;
 
    % coarse grid correction
-   xc = 2/(201) * [1:100]';
-   J = lin_interp_mat(x, xc);
-   Ac = J' * A * J;
-   ec = J * inv(Ac) * J' * r;
-   u = u + ec;
-   r = b - A*u;
+   % ec = J_3d * (Aci_3d * (J_3d' * r));
+   % u = u + ec;
+   % r = b - A_3d*u;
 
-   if norm(e)/norm(ue) <= 1e-8
+   err = norm(e)/norm(ue)
+   if err <= 1e-8
      k
-   endif
+   end;
+
 
 end;
-
-plot(LS,0*LS,'k-',LS,eh,'r-',lw,3); hold on;
-title(['Jac: $k$ = ' num2str([k-1 n]) ', $\omega$ = ' num2str(omega)],intp,ltx,fs,22);
-xlabel(['$\lambda(D^{-1}A)$'],intp,ltx,fs,18);
-ylabel(['${\hat e_k}/\|{\underline u}\|$'],intp,ltx,fs,18);
--- lin_interp_mat.m
+++ lin_interp_mat.m
@@ -1,59 +1,18 @@
-hdr;
-
-n=200; h=1/(n+1); h2i=1./(h*h);
-
-x = h*[1:n]';
-e = ones(n,1);
-A = h2i*spdiags([-e 2*e -e], -1:1, n, n);
-
-k = [1:n]';
-V = sqrt(2*h)*sin((h*pi)*(k*k'));
-Lam = (2*h2i)*(1-cos(h*pi*k));
-lmax = 2;
-lmin = 0.6;
-
-di = h*h/2;
-LS = di*Lam;
-
-ue=0*x;
-ue=rand(n,1);
-ue=V*e;
+function[J] = lin_interp_mat(xo,xi)
+   %
+   %     Compute the interpolation matrix from xi to xo
+   %
+   [xs,is]=sort(xo); m=length(xo);n=length(xi);
+   J=spalloc(m,n,3*m);
 
 
-b = A*ue; u=0*b; nsmooth=4; omega=2/3;
-
-r=b;
-for k=1:20
-   e=ue-u; eh=V'*e; uh=V'*u;
-   %plot(LS,0*LS,'k-',LS,eh,'r-',lw,3);
-   %title(['Jac: $k$ = ' num2str([k-1 n]) ', $\omega$ = ' num2str(omega)],intp,ltx,fs,22);
-   %xlabel(['$\lambda(D^{-1}A)$'],intp,ltx,fs,18);
-   %ylabel(['${\hat e_k}/\|{\underline u}\|$'],intp,ltx,fs,18);
-
-   %pause(.05 + .95/k);
-   %plot(LS,0*LS,'k-',LS,eh,'b-',lw,3); hold on;
-
-%  u=u+cheb_smooth(r,lmax,lmin,nsmooth,A);
-   for nu=1:nsmooth;
-      u=u+omega*di*r;
-      r=b-A*u;
+   j0=1;j1=2;x0=xi(j0);x1=xi(j1);
+   for i=1:m;
+      while xs(i) > x1;
+         j0=j1; j1=j0+1;;x0=x1;x1=xi(j1);
+      end;
+         J(i,j0)=(xs(i)-x1)/(x0-x1);
+         J(i,j1)=1-J(i,j0);
    end;
-
-   % coarse grid correction
-   xc = 2/(201) * [1:100]';
-   J = lin_interp_mat(x, xc);
-   Ac = J' * A * J;
-   ec = J * inv(Ac) * J' * r;
-   u = u + ec;
-   r = b - A*u;
-
-   if norm(e)/norm(ue) <= 1e-8
-     k
-   endif
-
-end;
-
-plot(LS,0*LS,'k-',LS,eh,'r-',lw,3); hold on;
-title(['Jac: $k$ = ' num2str([k-1 n]) ', $\omega$ = ' num2str(omega)],intp,ltx,fs,22);
-xlabel(['$\lambda(D^{-1}A)$'],intp,ltx,fs,18);
-ylabel(['${\hat e_k}/\|{\underline u}\|$'],intp,ltx,fs,18);
+   J(is,:)=J(:,:);
+   
