207deb006614e97366c99b7c9f4ad200b07b9ebd
ryan42210 <ryan_095@foxmail.com>
2024-11-18 16:52:54 -0600

    fix_mg_plot

--- mg_plot.m
+++ mg_plot.m
@@ -1,5 +1,5 @@
-n = 20;
-n_iter = 10;
+n = 15;
+n_iter = 50;
 ue = rand(n^3,1);
 
 y1 = aniso_multigrid_plot(n, n_iter, ue, 1, 1, 1);
@@ -8,5 +8,8 @@ y3 = aniso_multigrid_plot(n, n_iter, ue, 1, 1, 4);
 y4 = aniso_multigrid_plot(n, n_iter, ue, 1, 1, 8);
 x = 1:n_iter;
 
-semilogy(x, y1, x, y2, x, y3, x, y4)
-legend({'1','2', '3', '4'},'Location','southwest')
+
+semilogy(x, y1, 'Linewidth', 2,  x, y2, 'Linewidth', 2, x, y3, 'Linewidth', 2, x, y4, 'Linewidth', 2);
+legend({'r=1','r=2', 'r=4', 'r=8'},'Location','southwest')
+ylabel('norm of residual');
+xlabel('number of iterations')
